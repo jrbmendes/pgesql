d6bb17dfc4312d3af42deafdb65bde51d4dd7621
JR Mendes <barrocaj@pge.rj.gov.br>
2024-08-06 21:03:20 -0300

    Your commit message here

--- p4-3.bat
+++ p4-3.bat
@@ -1,4 +1,4 @@
-oc new-build https://github.com/jrbmendes/pgesql.git#main --name=pgesql-dev --strategy=docker --dockerfile=Containerfile --allow-missing-imagestream-tags
+oc new-build https://github.com/jrbmendes/pgesql.git#main --name=pgesql-dev --strategy=docker  --allow-missing-imagestream-tags
 
 oc start-build pgesql-dev --follow
 
